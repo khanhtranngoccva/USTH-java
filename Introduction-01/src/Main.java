47262148ea592688649410199b876ce7d27120cc
Khanh Tran <khanhtranngoccva@gmail.com>
2022-09-22 09:12:12 +0700

    initial: Initial commit

---
+++ Introduction-01/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int x = 1 + 2;
+        System.out.println(x);
+    }
+}
+
+class Main2 {
+
+}
